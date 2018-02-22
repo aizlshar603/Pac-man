6c4e9e70142fabbedb53fcce91b7d317678c92a5
temp <temp>
2018-02-22 12:53:25 -0500

    Add project files.

---
+++ Pac Man/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Pac_Man
+{
+    partial class PacMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayGame
+            // 
+            this.PlayGame.BackColor = System.Drawing.Color.White;
+            this.PlayGame.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.PlayGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayGame.Location = new System.Drawing.Point(101, 210);
+            this.PlayGame.Name = "PlayGame";
+            this.PlayGame.Size = new System.Drawing.Size(91, 39);
+            this.PlayGame.TabIndex = 0;
+            this.PlayGame.Text = "Run";
+            this.PlayGame.UseVisualStyleBackColor = false;
+            this.PlayGame.Click += new System.EventHandler(this.PlayGame_Click);
+            // 
+            // PacMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(305, 261);
+            this.Controls.Add(this.PlayGame);
+            this.Name = "PacMan";
+            this.Text = "PacMan!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayGame;
+    }
+}
+
---
+++ Pac Man/Form1.cs
@@ -0,0 +1,169 @@
+﻿///pac man
+///by Aizlea
+///Feb. 2018
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Pac_Man
+{
+    public partial class PacMan : Form
+    {
+        public PacMan()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayGame_Click(object sender, EventArgs e)
+        {
+            //clear button
+            Graphics g = this.CreateGraphics();
+            PlayGame.Visible = false;
+            g.Clear(Color.Black);
+              
+            //draw lines 
+            Pen drawPen = new Pen(Color.Blue, 10);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            
+            //draw pac man 
+            SolidBrush drawBrush = new SolidBrush(Color.Yellow);
+            g.FillPie(drawBrush, 20, 20, 50, 50, 30, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            
+            //moving pacman to the right
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 20, 20, 50, 50, 30, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 40, 20, 50, 50, 30, 360);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 60, 20, 50, 50, 30, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 80, 20, 50, 50, 30, 360);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 100, 20, 50, 50, 30, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 120, 20, 50, 50, 30, 360);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 140, 20, 50, 50, 30, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 160, 20, 50, 50, 30, 360);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 180, 20, 50, 50, 30, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 200, 20, 50, 50, 30, 360);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            //moving down
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 200, 20, 50, 50, 80, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 200, 20, 50, 50, 100, 360);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 200, 40, 50, 50, 100, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 200, 60, 50, 50, 100, 360);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 200, 80, 50, 50, 100, 300);
+            Thread.Sleep(300);
+            g.Clear(Color.Black);
+            g.DrawLine(drawPen, 0, 10, 275, 10);
+            g.DrawLine(drawPen, 0, 80, 175, 80);
+            g.DrawLine(drawPen, 275, 10, 275, 150);
+            g.DrawLine(drawPen, 175, 80, 175, 150);
+            g.FillPie(drawBrush, 200, 100, 50, 50, 100, 360);
+            
+            //End of game
+            Font drawFont = new Font("Arial", 16, FontStyle.Bold);
+            drawBrush.Color = Color.Red;
+            g.DrawString ("Game Over", drawFont, drawBrush ,50, 180);
+            Thread.Sleep(1000);
+            g.Clear(Color.Black);
+
+
+        }
+    }
+}
